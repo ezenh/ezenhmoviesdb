16325e144c2ea22fb402187b9e71fc007627f4c4
ezenh <ezenh87@gmail.com>
2024-03-19 01:55:10 -0300

    working on trends page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 let lang = 'es'
+let homePageDisplay = document.getElementById('body')
+console.log()
+// let homeOption = document.getElementById('homeOption')
+//       homeOption.addEventListener('click', completeWindow)
+let trendsOption = document.getElementById('trendsOption')
+      // trendingOption.addEventListener('click', completeWindow)
+let categoriesOption = document.getElementById('categoriesOption')
+      // categoriesOption.addEventListener('click', completeWindow)
+let popularOption = document.getElementById('popularOption')
+      // popularOption.addEventListener('click', completeWindow)
+let upcomingOption = document.getElementById('upcomingOption')
+      // upcomingOption.addEventListener('click', completeWindow)
+let searchOption = document.getElementById('searchOption')
+      // searchOption.addEventListener('click', completeWindow)
 
 const API_URL = axios.create({
       baseURL: 'https://api.themoviedb.org/3',
@@ -19,31 +33,43 @@ let upcomming_URL = '/movie/upcoming'
 
 async function getConfiguration () {
       const response = await API_URL(configuration_URL)
-      console.log(response)
+      // console.log(response)
 }
 getConfiguration()
+
 async function getMainMoviesPreview() {
       const response = await API_URL(trends_URL)
       let movies = response.data.results
-      console.log(movies[0])
+      // console.log(movies[0])
+      // console.log(typeof response)
 
       let mainMovies = document.getElementById('main-movies--container')
       let body = ''
       for(let i = 0; i < 5; i++) {
             body +=
-            `<article id= '${movies[i].id}' class='main-movie' style='background-image: url("https://image.tmdb.org/t/p/original${movies[i].backdrop_path}")'>
-                  <div>Movie info</div>
-                  <h2>${movies[i].title}</h2>
-                  <p>${movies[i].overview}</p>
-                  <div class="main-movie-buttons">
-                        <button class="main-movie-trailer"><img src="icons/play.png" alt=""> TRAILER</button>
-                        <button class="main-movie-details"><img src="icons/info.png" alt=""> DETALLES</button>
+            `<article id= '${movies[i].id}' class='main-movie-article' style= 'background-image: url("https://image.tmdb.org/t/p/original${movies[i].backdrop_path}")'>
+                  <div class='main-movie'>
+                        <div>Movie info</div>
+                        <h2>${movies[i].title}</h2>
+                        <p>${movies[i].overview}</p>
+                        <div class="main-movie-buttons">
+                              <button class="main-movie-trailer"><img src="icons/play.png" alt="">TRAILER</button>
+                              <button class="main-movie-details"><img src="icons/info.png" alt="">DETALLES</button>
+                        </div>
                   </div>
             </article>`
             }
       mainMovies.innerHTML = body
+
       count = 0
       direction = 'left'
+      
+      dot1 = document.getElementById('dot1')
+      dot2 = document.getElementById('dot2')
+      dot3 = document.getElementById('dot3')
+      dot4 = document.getElementById('dot4')
+      dot5 = document.getElementById('dot5')
+      dot1.style.backgroundColor = 'white'
 
       setInterval(function changePic(){
             if(count == 0) {
@@ -52,7 +78,6 @@ async function getMainMoviesPreview() {
             if(count == 4) {
                   direction = 'right'
                   }
-       
             if(direction == 'left') {
                   mainMovies.scrollBy({
                         top: 0,
@@ -60,8 +85,6 @@ async function getMainMoviesPreview() {
                         behavior:'smooth'
                   })
                   count++
-                  console.log(count)
-                  console.log(direction)
                   }
             if(direction == 'right') {
                   mainMovies.scrollBy({
@@ -70,12 +93,45 @@ async function getMainMoviesPreview() {
                         behavior:'smooth'
                   })
                   count--
-                  console.log(count)   
-                  console.log(direction)
                   }
-      },8000)
+                  // console.log(count)
+            if(count == 0) {
+                  dot1.style.backgroundColor = 'white'
+                  dot2.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot3.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot4.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot5.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  }
+            if(count == 1) {
+                  dot1.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot2.style.backgroundColor = 'white'
+                  dot3.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot4.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot5.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  }
+            if(count == 2) {
+                  dot1.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot2.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot3.style.backgroundColor = 'white'
+                  dot4.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot5.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  }
+            if(count == 3) {
+                  dot1.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot2.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot3.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot4.style.backgroundColor = 'white'
+                  dot5.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  }
+            if(count == 4 || count == 5) {
+                  dot1.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot2.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot3.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot4.style.backgroundColor = 'rgb(128,128,128, 0.3)'
+                  dot5.style.backgroundColor = 'white'
+                  }
+      },2000)
 }
-
 getMainMoviesPreview()
 
 async function getTrendingMoviesPreview() {
@@ -143,3 +199,17 @@ async function getUpcomingMoviesPreview() {
       upcoming.innerHTML = body
 }
 getUpcomingMoviesPreview()
+
+// function completeWindow(event) {
+//       let quitWindow = document.getElementById('quitWindow')
+//             quitWindow.addEventListener('click', function() {
+//                   window.location.hash = '#home';
+//                   windowOption.style.display = 'none'
+//                   })
+//       let windowOption = document.getElementById('navigation--container')
+//             windowOption.style.display = 'flex'
+
+//       // console.log(event.target)
+//       // console.log(event.target.id)
+//       // console.log(location.hash)
+// }
--- src/navigation.js
+++ src/navigation.js
@@ -3,30 +3,118 @@ window.addEventListener('hashchange', navigator, false)
 
 
 function navigator() {
-console.log({location})
-
 if (location.hash.startsWith('#trends')) {
     trendsPage()
-}
+    }
 if (location.hash.startsWith('#categories')) {
     categoriesPage()
-}
+    }
 if (location.hash.startsWith('#popular')) {
     popularPages()
-}
+    }
 if (location.hash.startsWith('#upcomming')) {
     upcommingPage()
-}
-else{
-    homepage()}
+    }
+if (location.hash.startsWith('#search')) {
+    searchPage()
+    }
+if (location.hash.startsWith('#home')) {
+    homePage()
+    }
 }
 
-function homepage(){
+function homePage(){
     console.log('Estas en Home!')
     }
 
-function trendsPage(){
+let pageNumber = 1
+async function trendsPage(){
     console.log('Estás en Trends!')
+
+    let windowOption = document.getElementById('navigation--container')
+        windowOption.style.display = 'flex'
+        windowOption.style.height = String(homePageDisplay.offsetHeight + 'px')
+        console.log(homePageDisplay.offsetHeight)
+    let windowTitle = document.getElementById('navigation-title')
+        windowTitle.innerText = 'Trends'
+    let navigationContent = document.getElementById('navigation-content')
+
+    let quitWindow = document.getElementById('quitWindow')
+        quitWindow.addEventListener('click', function() {
+            window.location.hash = '#home';
+            windowOption.style.display = 'none'
+        })
+    
+    let movies = ''
+    let pages = ''
+
+    if(pageNumber==1) {
+        loadContent()
+        loadPages()
+    }
+    let forwardPageButton = document.getElementById('forward')
+        forwardPageButton.addEventListener('click', function backPage() {
+            if(pageNumber < pages) {
+                console.log('una mas')
+                pageNumber = pageNumber + 1
+                loadContent()
+                }
+    })
+    let backPageButton = document.getElementById('back')
+        backPageButton.addEventListener('click', function backPage() {
+        if(pageNumber > 1) {
+            console.log('una menos')
+                pageNumber = pageNumber - 1
+                loadContent()
+                }
+    })
+    async function loadContent() {
+        const response = await API_URL(trends_URL, {params:{page: String(pageNumber)}})
+        movies = response.data.results
+        pages = response.data.total_pages
+    let moviesBody = ''
+    movies.forEach(movie => {
+        moviesBody +=
+        `<article id= '${movie.id}'>
+                <img alt='${movie.title}' src='https://image.tmdb.org/t/p/w300${movie.poster_path}'>
+                <p>${movie.title}</p>
+        </article>`
+        })
+    navigationContent.innerHTML = moviesBody
+    if(pageNumber == 1) {
+        loadPages()
+    }
+    }
+
+    async function loadPages() {
+    let navigationPages = document.getElementById('pages')
+
+
+    for (let i = 1; i <= pages; i++) {
+        if(i == pages) {
+            let pageNumberLink = document.createElement('a')
+                navigationPages.appendChild(pageNumberLink)
+                pageNumberLink.className = 'pageNumberLink'
+                pageNumberLink.id = 'page' + i
+                pageNumberLink.innerText = i
+                pageNumberLink.addEventListener('click', function goToPage(event) {
+                    console.log(event.target.innerText)
+                    pageNumber = event.target.innerText
+                    loadContent()
+                })
+        }else{
+            let pageNumberLink = document.createElement('a')
+                navigationPages.appendChild(pageNumberLink)
+                pageNumberLink.className = 'pageNumberLink'
+                pageNumberLink.id = 'page' + i
+                pageNumberLink.innerText = i
+                pageNumberLink.addEventListener('click', function goToPage(event) {
+                    console.log(event.target.innerText)
+                    pageNumber = event.target.innerText
+                    loadContent()
+                })
+            }
+    }}
     }
 
 function categoriesPage(){
@@ -39,4 +127,8 @@ function popularPages(){
 
 function upcommingPage(){
     console.log('Estás en Próximos Estrenos!')
+    }
+
+function searchPage(){
+    console.log('Estás en Búsqueda!')
     }
